404d5e7bf087cceeeaa21b3c1f2d9ab12f3d65c1
Michael Wang <ghzlmc@gmail.com>
2026-05-27 22:47:41 +0800

    Initial Commit

---
+++ Why AwakeTests/Why_AwakeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Why_AwakeTests.swift
+//  Why AwakeTests
+//
+//  Created by Michael Wang on 5/27/26.
+//
+
+import Testing
+@testable import Why_Awake
+
+struct Why_AwakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
